2f6d51f62943a628bbb220e16b08ede9d1606022
Gisselle Garcia <gissellegar46@gmail.com>
2023-05-01 11:59:48 -0700

    Attempt mock data based on old project, comment out while qus tions are answered

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 const path = require('path');
 const db = require('./db/db-connection.js');
-
+//const mockdata = require('./server/data');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -217,84 +217,14 @@ app.post('/api/goals', async (req, res) => {
 //       res.json(requestedImages);
 //   });
 // });
-  
 
 
-//API MOCK DATA
-// {
-//     "page": 1,
-//     "per_page": 3,
-//     "photos": [
-//         {
-//             "id": 15286,
-//             "width": 2500,
-//             "height": 1667,
-//             "url": "https://www.pexels.com/photo/person-walking-between-green-forest-trees-15286/",
-//             "photographer": "Luis del Río",
-//             "photographer_url": "https://www.pexels.com/@luisdelrio",
-//             "photographer_id": 1081,
-//             "avg_color": "#283419",
-//             "src": {
-//                 "original": "https://images.pexels.com/photos/15286/pexels-photo.jpg",
-//                 "large2x": "https://images.pexels.com/photos/15286/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-//                 "large": "https://images.pexels.com/photos/15286/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=650&w=940",
-//                 "medium": "https://images.pexels.com/photos/15286/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350",
-//                 "small": "https://images.pexels.com/photos/15286/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=130",
-//                 "portrait": "https://images.pexels.com/photos/15286/pexels-photo.jpg?auto=compress&cs=tinysrgb&fit=crop&h=1200&w=800",
-//                 "landscape": "https://images.pexels.com/photos/15286/pexels-photo.jpg?auto=compress&cs=tinysrgb&fit=crop&h=627&w=1200",
-//                 "tiny": "https://images.pexels.com/photos/15286/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=1&fit=crop&h=200&w=280"
-//             },
-//             "liked": false,
-//             "alt": "Person Walking Between Green Forest Trees"
-//         },
-//         {
-//             "id": 3408744,
-//             "width": 3546,
-//             "height": 2255,
-//             "url": "https://www.pexels.com/photo/scenic-view-of-snow-capped-mountains-during-night-3408744/",
-//             "photographer": "stein egil liland",
-//             "photographer_url": "https://www.pexels.com/@therato",
-//             "photographer_id": 144244,
-//             "avg_color": "#557088",
-//             "src": {
-//                 "original": "https://images.pexels.com/photos/3408744/pexels-photo-3408744.jpeg",
-//                 "large2x": "https://images.pexels.com/photos/3408744/pexels-photo-3408744.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-//                 "large": "https://images.pexels.com/photos/3408744/pexels-photo-3408744.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
-//                 "medium": "https://images.pexels.com/photos/3408744/pexels-photo-3408744.jpeg?auto=compress&cs=tinysrgb&h=350",
-//                 "small": "https://images.pexels.com/photos/3408744/pexels-photo-3408744.jpeg?auto=compress&cs=tinysrgb&h=130",
-//                 "portrait": "https://images.pexels.com/photos/3408744/pexels-photo-3408744.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=1200&w=800",
-//                 "landscape": "https://images.pexels.com/photos/3408744/pexels-photo-3408744.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=627&w=1200",
-//                 "tiny": "https://images.pexels.com/photos/3408744/pexels-photo-3408744.jpeg?auto=compress&cs=tinysrgb&dpr=1&fit=crop&h=200&w=280"
-//             },
-//             "liked": false,
-//             "alt": "Scenic View Of Snow Capped Mountains During Night"
-//         },
-//         {
-//             "id": 572897,
-//             "width": 6914,
-//             "height": 4463,
-//             "url": "https://www.pexels.com/photo/mountain-covered-snow-under-star-572897/",
-//             "photographer": "eberhard grossgasteiger",
-//             "photographer_url": "https://www.pexels.com/@eberhardgross",
-//             "photographer_id": 121938,
-//             "avg_color": "#5D5A63",
-//             "src": {
-//                 "original": "https://images.pexels.com/photos/572897/pexels-photo-572897.jpeg",
-//                 "large2x": "https://images.pexels.com/photos/572897/pexels-photo-572897.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-//                 "large": "https://images.pexels.com/photos/572897/pexels-photo-572897.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
-//                 "medium": "https://images.pexels.com/photos/572897/pexels-photo-572897.jpeg?auto=compress&cs=tinysrgb&h=350",
-//                 "small": "https://images.pexels.com/photos/572897/pexels-photo-572897.jpeg?auto=compress&cs=tinysrgb&h=130",
-//                 "portrait": "https://images.pexels.com/photos/572897/pexels-photo-572897.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=1200&w=800",
-//                 "landscape": "https://images.pexels.com/photos/572897/pexels-photo-572897.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=627&w=1200",
-//                 "tiny": "https://images.pexels.com/photos/572897/pexels-photo-572897.jpeg?auto=compress&cs=tinysrgb&dpr=1&fit=crop&h=200&w=280"
-//             },
-//             "liked": false,
-//             "alt": "Mountain Covered Snow Under Star"
-//         }
-//     ],
-//     "total_results": 8000,
-//     "next_page": "https://api.pexels.com/v1/search/?page=2&per_page=3&query=nature"
-// }
+//mockdata get request
+// app.get("/api/pexels/", (req, res) => {
+//       res.json(mockdata);
+//   });
+
+
 
 //*************************************************************************************************************************************** */
 
