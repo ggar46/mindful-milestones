b3af78d5c481611db29fbb6a9233b261acec4dbd
Gisselle Garcia <gissellegar46@gmail.com>
2023-05-03 21:11:25 -0700

    Import NavBar to Goals link page

--- client/src/components/Goals.jsx
+++ client/src/components/Goals.jsx
@@ -1,11 +1,14 @@
 import FormGoal from "./FormGoal";
+import MyNavBar from "./Navbar";
 
 const Goals = () => {
 
     return(
         <div>
+            <MyNavBar/>
             <h1>Goals page!</h1>
             <FormGoal/>
+
         </div>
     )
 }
