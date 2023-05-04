65aad3630f266e769a5b498a99ce5eb139764ec4
Gisselle Garcia <gissellegar46@gmail.com>
2023-05-03 20:42:38 -0700

    Create and render browser router in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "http://localhost:5173/Home",
+    element: <Home/>,
+  },
+  {
+    path: "http://localhost:5173/Goals",
+    element: <Goals/>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </React.StrictMode>,
 )
