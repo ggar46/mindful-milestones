89524ecc589ebcbe494a09699118a78f10753f11
Gisselle Garcia <gissellegar46@gmail.com>
2023-05-03 20:46:43 -0700

    Import Home and Goals component into main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import Home from './components/Home'
+import Goals from './components/Goals'
 import {
   createBrowserRouter,
   RouterProvider,
