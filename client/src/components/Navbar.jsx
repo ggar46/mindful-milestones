1a683ef6b86353f5ae92f27c9843d878342a9297
Gisselle Garcia <gissellegar46@gmail.com>
2023-05-03 20:34:48 -0700

    Add links to NavBar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -18,7 +18,10 @@ function MyNavBar(props) {
               alt="React Bootstrap logo"
             />
         </Navbar.Brand>
-        <Nav.Link >Your Link</Nav.Link>
+        <Nav.Link >Home</Nav.Link>
+        <Nav.Link >Goals</Nav.Link>
+        
+
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
           <Navbar.Text>
