d8e51fcbb93b28ea9bb4c9ccc9c8d863cfb23321
Gisselle Garcia <gissellegar46@gmail.com>
2023-05-21 22:06:58 -0700

    Add conditional code checking authentification before displaying log-in navbar to avoid duplicate navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './components/Home';
+import MyNavBar from './components/Navbar';
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
 
+  const {isAuthenticated} = useAuth0();
   return (
     <div data-testid="taskModal" className="App">
+      {!isAuthenticated ? <MyNavBar  /> : null}
       <Home/>
     </div>
   )
