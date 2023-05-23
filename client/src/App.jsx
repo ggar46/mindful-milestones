1889c8515dc28c69f973e86997b6798f89fe2354
Gisselle Garcia <gissellegar46@gmail.com>
2023-05-23 15:23:53 -0700

    Display login page instead of navbar if there is no authentification

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './components/Home';
 import MyNavBar from './components/Navbar';
 import { useAuth0 } from "@auth0/auth0-react";
+import Login from './components/Login';
 
 function App() {
 
   const {isAuthenticated} = useAuth0();
   return (
     <div data-testid="taskModal" className="App">
-      {!isAuthenticated ? <MyNavBar  /> : null}
+      {!isAuthenticated ? <Login  /> : null}
       <Home/>
     </div>
   )
