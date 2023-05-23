e7b41daea74b29e88e997cdbda9a35db2ab9aa18
Gisselle Garcia <gissellegar46@gmail.com>
2023-05-23 16:25:26 -0700

    Complete css for login page

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import brainWebLogo from "../assets/brainWebLogo.png";
+import brainNoBg from "../assets/brainNoBg.png";
+import nobgLogoTitle from "../assets/nobgLogoTitle.png";
 
 const Login = () => {
   const { loginWithRedirect } = useAuth0();
   return (
     <div id="homepage">
     <div id="homepage-content">
+    <img width="450" src={nobgLogoTitle}></img>
       <div className="homepagetitle">
-        <img width="400" src={brainWebLogo}></img>
+        <img width="200" src={brainNoBg}></img>
       </div>
       <p className="description">
         {" "}
